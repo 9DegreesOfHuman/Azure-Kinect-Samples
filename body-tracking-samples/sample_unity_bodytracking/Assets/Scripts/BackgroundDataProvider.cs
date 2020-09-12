67dd903549d26e00ad73f9d45ac2b3c6caf5e65b
Hololens Dev Pc <bitter.daniel@gmail.com>
2020-09-12 13:59:05 -0500

    clean comments

--- body-tracking-samples/sample_unity_bodytracking/Assets/Scripts/BackgroundDataProvider.cs
+++ body-tracking-samples/sample_unity_bodytracking/Assets/Scripts/BackgroundDataProvider.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Threading.Tasks;
-
-public abstract class BackgroundDataProvider
+﻿public abstract class BackgroundDataProvider
 {
     // protected volatile bool m_runBackgroundThread;
     private BackgroundData m_frameBackgroundData = new BackgroundData();
-    // public bool IsRunning { get; set; } = false;
-
     public abstract void StartClientThread(int id);
 
     protected abstract void RunBackgroundThreadAsync(int id);
-
-    // public void StopClientThread()
-    // {
-    //     UnityEngine.Debug.Log("Stopping BackgroundDataProvider thread.");
-    //     // m_runBackgroundThread = false;
-    // }
 }
