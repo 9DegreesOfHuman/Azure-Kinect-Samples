89fcc11812cede3c473c9c0451850dcb639b557b
Hololens Dev Pc <bitter.daniel@gmail.com>
2020-09-12 14:43:27 -0500

    Works for printing

--- body-tracking-samples/sample_unity_bodytracking/Assets/Scripts/main.cs
+++ body-tracking-samples/sample_unity_bodytracking/Assets/Scripts/main.cs
@@ -13,69 +13,158 @@ public class main : MonoBehaviour
 
     protected void RunBackgroundThreadAsync()
     {
+        Device device = null;
+        Calibration deviceCalibration = new Calibration();
         try
         {
-            UnityEngine.Debug.Log("Starting body tracker background thread.");
+            const int id = 0;
+            device = Device.Open(id);
+            device.StartCameras(new DeviceConfiguration()
+            {
+                CameraFPS = FPS.FPS30,
+                ColorResolution = ColorResolution.Off,
+                DepthMode = DepthMode.NFOV_Unbinned,
+                WiredSyncMode = WiredSyncMode.Standalone,
+            });
+
+            UnityEngine.Debug.Log("Open K4A device successful. id " + id + "sn:" + device.SerialNum);
+
+            deviceCalibration = device.GetCalibration();
+
+        } catch (Exception e)
+        {
+            UnityEngine.Debug.LogError(e.Message);
+        }
+
+        Tracker tracker = null;
+        try
+        {
+            TrackerConfiguration calibration = new TrackerConfiguration() {
+                ProcessingMode = TrackerProcessingMode.Gpu,
+                SensorOrientation = SensorOrientation.Default
+            };
+            tracker = Tracker.Create(deviceCalibration, calibration);
+
+            UnityEngine.Debug.Log("Body tracker created.");
+        } catch (Exception e)
+        {
+            UnityEngine.Debug.LogError(e.Message);
+        }
+        
+
+        bool running = true;
+        while(running)
+        {
+            try
+            {
+                Capture sensorCapture = device.GetCapture();
+                tracker.EnqueueCapture(sensorCapture);
+            }
+            catch (Exception e)
+            {
+                UnityEngine.Debug.LogError(e.Message);
+            }
+
+            Frame frame = null;
+            try
+            {
+                frame = tracker.PopResult(TimeSpan.Zero, throwOnTimeout: false);
+                if (frame == null)
+                {
+                    UnityEngine.Debug.Log("Pop result from tracker timeout!");
+                    continue;
+                }
+
+                if (frame.NumberOfBodies != 1) {
+                    _print(true, $"Non-singlular # of bodies: {frame.NumberOfBodies}");
+                    continue;
+                }
+
+                Microsoft.Azure.Kinect.BodyTracking.Body body = frame.GetBody(0);
+                Microsoft.Azure.Kinect.BodyTracking.Skeleton skeleton = frame.GetBodySkeleton(0);
+
+                int numJoints = Microsoft.Azure.Kinect.BodyTracking.Skeleton.JointCount;
+                // _print(true, $"numJoints: {numJoints}"); // 32
+                for (int jointId = 0; jointId < numJoints; jointId++)
+                {
+                    Microsoft.Azure.Kinect.BodyTracking.Joint joint = skeleton.GetJoint(jointId);
+                    System.Numerics.Vector3 positionVector3 = joint.Position;
+                    var pos = joint.Position;
+                    // _print(true, "pos: " + (JointId)jointId + " " + pos[0] + " " + pos[1] + " " + pos[2]);
+                    _print(true, "pos: " + (JointId)jointId + " " + pos.X + " " + pos.Y + " " + pos.Z);
+                }
+            }
+            catch (Exception e)
+            {
+                UnityEngine.Debug.LogError(e.Message);
+            }
+        }
+
+        // tracker.Dispose();
+        // device.Dispose();
+        // try
+        {
+            // UnityEngine.Debug.Log("Starting body tracker background thread.");
 
             // Buffer allocations.
             // Open device.
-            const int id = 0;
-            using (Device device = Device.Open(id))
+            // const int id = 0;
+            // using (Device device = Device.Open(id))
             {
-                device.StartCameras(new DeviceConfiguration()
+                // device.StartCameras(new DeviceConfiguration()
+                // {
+                //     CameraFPS = FPS.FPS30,
+                //     ColorResolution = ColorResolution.Off,
+                //     DepthMode = DepthMode.NFOV_Unbinned,
+                //     WiredSyncMode = WiredSyncMode.Standalone,
+                // });
+
+                // UnityEngine.Debug.Log("Open K4A device successful. id " + id + "sn:" + device.SerialNum);
+
+                // var deviceCalibration = device.GetCalibration();
+
+                // using (Tracker tracker = Tracker.Create(deviceCalibration, new TrackerConfiguration() { ProcessingMode = TrackerProcessingMode.Gpu, SensorOrientation = SensorOrientation.Default }))
                 {
-                    CameraFPS = FPS.FPS30,
-                    ColorResolution = ColorResolution.Off,
-                    DepthMode = DepthMode.NFOV_Unbinned,
-                    WiredSyncMode = WiredSyncMode.Standalone,
-                });
-
-                UnityEngine.Debug.Log("Open K4A device successful. id " + id + "sn:" + device.SerialNum);
-
-                var deviceCalibration = device.GetCalibration();
-
-                using (Tracker tracker = Tracker.Create(deviceCalibration, new TrackerConfiguration() { ProcessingMode = TrackerProcessingMode.Gpu, SensorOrientation = SensorOrientation.Default }))
-                {
-                    UnityEngine.Debug.Log("Body tracker created.");
+                    // UnityEngine.Debug.Log("Body tracker created.");
                     // while (m_runBackgroundThread)
-                    while (true)
+                    // while (true)
                     {
-                        using (Capture sensorCapture = device.GetCapture())
+                        // using (Capture sensorCapture = device.GetCapture())
                         {
                             // Queue latest frame from the sensor.
-                            tracker.EnqueueCapture(sensorCapture);
+                            // tracker.EnqueueCapture(sensorCapture);
                         }
 
                         // Try getting latest tracker frame.
-                        using (Frame frame = tracker.PopResult(TimeSpan.Zero, throwOnTimeout: false))
+                        // using (Frame frame = tracker.PopResult(TimeSpan.Zero, throwOnTimeout: false))
                         {
-                            if (frame == null)
-                            {
-                                UnityEngine.Debug.Log("Pop result from tracker timeout!");
-                            }
-                            else
+                            // if (frame == null)
+                            // {
+                            //     UnityEngine.Debug.Log("Pop result from tracker timeout!");
+                            // }
+                            // else
                             {
                                 // IsRunning = true;
                                 // Get number of bodies in the current frame.
                                 // currentFrameData.NumOfBodies = frame.NumberOfBodies;
 
-                                if (frame.NumberOfBodies != 1) { _print(true, $"Non-singlular # of bodies: {frame.NumberOfBodies}"); }
-                                if (frame.NumberOfBodies == 1)
-                                {
-                                    Microsoft.Azure.Kinect.BodyTracking.Body body = frame.GetBody(0);
-                                    Microsoft.Azure.Kinect.BodyTracking.Skeleton skeleton = frame.GetBodySkeleton(0);
+                                // if (frame.NumberOfBodies != 1) { _print(true, $"Non-singlular # of bodies: {frame.NumberOfBodies}"); }
+                                // if (frame.NumberOfBodies == 1)
+                                // {
+                                //     Microsoft.Azure.Kinect.BodyTracking.Body body = frame.GetBody(0);
+                                //     Microsoft.Azure.Kinect.BodyTracking.Skeleton skeleton = frame.GetBodySkeleton(0);
 
-                                    int numJoints = Microsoft.Azure.Kinect.BodyTracking.Skeleton.JointCount;
-                                    // _print(true, $"numJoints: {numJoints}"); // 32
-                                    for (int jointId = 0; jointId < numJoints; jointId++)
-                                    {
-                                        Microsoft.Azure.Kinect.BodyTracking.Joint joint = skeleton.GetJoint(jointId);
-                                        System.Numerics.Vector3 positionVector3 = joint.Position;
-                                        var pos = joint.Position;
-                                        // _print(true, "pos: " + (JointId)jointId + " " + pos[0] + " " + pos[1] + " " + pos[2]);
-                                        _print(true, "pos: " + (JointId)jointId + " " + pos.X + " " + pos.Y + " " + pos.Z);
-                                    }
-                                }
+                                //     int numJoints = Microsoft.Azure.Kinect.BodyTracking.Skeleton.JointCount;
+                                //     // _print(true, $"numJoints: {numJoints}"); // 32
+                                //     for (int jointId = 0; jointId < numJoints; jointId++)
+                                //     {
+                                //         Microsoft.Azure.Kinect.BodyTracking.Joint joint = skeleton.GetJoint(jointId);
+                                //         System.Numerics.Vector3 positionVector3 = joint.Position;
+                                //         var pos = joint.Position;
+                                //         // _print(true, "pos: " + (JointId)jointId + " " + pos[0] + " " + pos[1] + " " + pos[2]);
+                                //         _print(true, "pos: " + (JointId)jointId + " " + pos.X + " " + pos.Y + " " + pos.Z);
+                                //     }
+                                // }
                             }
 
                         }
@@ -85,10 +174,10 @@ public class main : MonoBehaviour
                 // device.Dispose();
             }
         }
-        catch (Exception e)
-        {
-            UnityEngine.Debug.LogError(e.Message);
-        }
+        // catch (Exception e)
+        // {
+        //     UnityEngine.Debug.LogError(e.Message);
+        // }
     }
 
     void _print(bool shouldPrint, string msg)
